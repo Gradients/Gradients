4549c53d5218d7f7356dca81306b11caf394f63c
Cruz <cruzdiary@gmail.com>
2018-09-30 15:50:23 +0900

    Add Direction.swift

---
+++ Direction.swift
@@ -0,0 +1,51 @@
+//
+//  Direction.swift
+//  Gradients
+//
+//  Created by Cruz on 30/09/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+import UIKit
+
+enum Direction {
+    case top
+    case left
+    case right
+    case bottom
+    case degree(CGFloat)
+}
+
+extension Direction {
+    var startPoint: CGPoint {
+        switch self {
+        case .top:
+            return CGPoint(x: 0.5, y: 1.0)
+        case .left:
+            return CGPoint(x: 1.0, y: 0.5)
+        case .right:
+            return CGPoint(x: 0.0, y: 0.5)
+        case .bottom:
+            return CGPoint(x: 0.5, y: 0.0)
+        case .degree(let degree):
+            let radian = degree * .pi / 180
+            return CGPoint(x: 0.5 * (cos(radian) + 1), y: 0.5 * (1 - sin(radian)))
+        }
+    }
+    
+    var endPoint: CGPoint {
+        switch self {
+        case .top:
+            return CGPoint(x: 0.5, y: 0.0)
+        case .left:
+            return CGPoint(x: 0.0, y: 0.5)
+        case .right:
+            return CGPoint(x: 1.0, y: 0.5)
+        case .bottom:
+            return CGPoint(x: 0.5, y: 1.0)
+        case .degree(let degree):
+            let radian = degree * .pi / 180
+            return CGPoint(x: 0.5 * (cos(radian + .pi) + 1), y: 0.5 * (1 + sin(radian)))
+        }
+    }
+}
