9cae71342ef1d9042be315e30c21a4635b676a1a
Cruz <cruzdiary@gmail.com>
2018-10-03 11:17:43 +0900

    Support Carthage

---
+++ Gradients/Gradients.h
@@ -0,0 +1,19 @@
+//
+//  Gradients.h
+//  Gradients
+//
+//  Created by Cruz on 03/10/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gradients.
+FOUNDATION_EXPORT double GradientsVersionNumber;
+
+//! Project version string for Gradients.
+FOUNDATION_EXPORT const unsigned char GradientsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gradients/PublicHeader.h>
+
+
