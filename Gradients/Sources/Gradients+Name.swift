812c0d0f4da89a7bcc8c576b295da45df7727ebd
Cruz <cruzdiary@gmail.com>
2018-10-13 23:08:33 +0900

    Add Gradients+Name

---
+++ Gradients/Sources/Gradients+Name.swift
@@ -0,0 +1,366 @@
+public extension Gradients {
+    public var name: String {
+        switch self {
+        case .warmFlame:
+           return "Warm Flame"
+        case .nightFade:
+            return "Night Fade"
+        case .springWarmth:
+            return "Spring Warmth"
+        case .juicyPeach:
+            return "Juicy Peach"
+        case .youngPassion:
+            return "Young Passion"
+        case .ladyLips:
+            return "Lady Lips"
+        case .sunnyMorning:
+            return "Sunny Morning"
+        case .rainyAshville:
+            return "Rainy Ashville"
+        case .frozenDreams:
+            return "Frozen Dreams"
+        case .winterNeva:
+            return "Winter Neva"
+        case .dustyGrass:
+            return "Dusty Grass"
+        case .temptingAzure:
+            return "Tempting Azure"
+        case .heavyRain:
+            return "Heavy Rain"
+        case .amyCrisp:
+            return "Amy Crisp"
+        case .meanFruit:
+            return "Mean Fruit"
+        case .softBlue:
+            return "Soft Blue"
+        case .ripeMalinka:
+            return "Ripe Malinka"
+        case .cloudyKnoxville:
+            return "Cloudy Knoxville"
+        case .malibuBeach:
+            return "Malibu Beach"
+        case .newLife:
+            return "NewLife"
+        case .trueSunset:
+            return "True Sunset"
+        case .morpheusDen:
+            return "Morpheus Den"
+        case .rareWind:
+            return "Rare Wind"
+        case .nearMoon:
+            return "Near Moon"
+        case .wildApple:
+            return "Wild Apple"
+        case .saintPetersburg:
+            return "Saint Petersburg"
+        case .ariellesSmile:
+            return "Arielles Smile"
+        case .plumPlate:
+            return "Plum Plate"
+        case .everlastingSky:
+            return "Everlasting Sky"
+        case .happyFisher:
+            return "Happy Fisher"
+        case .blessing:
+            return "Blessing"
+        case .sharpeyeEagle:
+            return "Sharpeye Eagle"
+        case .ladogaBottom:
+            return "Ladoga Bottom"
+        case .lemonGate:
+            return "Lemon Gate"
+        case .itmeoBranding:
+            return "Itmeo Branding"
+        case .zeusMiracle:
+            return "Zeus Miracle"
+        case .oldHat:
+            return "Old Hat"
+        case .starWine:
+            return "Star Wine"
+        case .deepBlue:
+            return "Deep Blue"
+        case .coupDeGrace:
+            return "Coup De Grace"
+        case .happyAcid:
+            return "Happy Acid"
+        case .awesomePine:
+            return "Awesome Pine"
+        case .newYork:
+            return "New York"
+        case .shyRainbow:
+            return "Shy Rainbow"
+        case .loonCrest:
+            return "Loon Crest"
+        case .mixedHopes:
+            return "Mixed Hopes"
+        case .flyHigh:
+            return "Fly High"
+        case .strongBliss:
+            return "Strong Bliss"
+        case .freshMilk:
+            return "Fresh Milk"
+        case .snowAgain:
+            return "Snow Again"
+        case .februaryInk:
+            return "February Ink"
+        case .kindSteel:
+            return "Kind Steel"
+        case .softGrass:
+            return "Soft Grass"
+        case .grownEarly:
+            return "Grown Early"
+        case .sharpBlues:
+            return "Sharp Blues"
+        case .shadyWater:
+            return "Shady Water"
+        case .dirtyBeauty:
+            return "Dirty Beauty"
+        case .greatWhale:
+            return "Great Whale"
+        case .teenNotebook:
+            return "Teen Notebook"
+        case .politeRumors:
+            return "Polite Rumors"
+        case .sweetPeriod:
+            return "Sweet Period"
+        case .wideMatrix:
+            return "Wide Matrix"
+        case .softCherish:
+            return "Soft Cherish"
+        case .redSalvation:
+            return "Red Salvation"
+        case .burningSpring:
+            return "Burning Spring"
+        case .nightParty:
+            return "Night Party"
+        case .skyGlider:
+            return "Sky Glider"
+        case .heavenPeach:
+            return "Heaven Peach"
+        case .purpleDivision:
+            return "Purple Division"
+        case .aquaSplash:
+            return "Aqua Splash"
+        case .aboveClouds:
+            return "Above Clouds"
+        case .spikyNaga:
+            return "Spiky Naga"
+        case .loveKiss:
+            return "Love Kiss"
+        case .sharpGlass:
+            return "Sharp Glass"
+        case .cleanMirror:
+            return "Clean Mirror"
+        case .premiumDark:
+            return "Premium Dark"
+        case .coldEvening:
+            return "Cold Evening"
+        case .cochitiLake:
+            return "Cochiti Lake"
+        case .summerGames:
+            return "Summer Games"
+        case .passionateBed:
+            return "Passionate Bed"
+        case .mountainRock:
+            return "Mountain Rock"
+        case .desertHump:
+            return "Desert Hump"
+        case .jungleDay:
+            return "Jungle Day"
+        case .phoenixStart:
+            return "Phoenix Start"
+        case .octoberSilence:
+            return "October Silence"
+        case .farawayRiver:
+            return "Faraway River"
+        case .alchemistLab:
+            return "Alchemist Lab"
+        case .overSun:
+            return "Over Sun"
+        case .premiumWhite:
+            return "Premium White"
+        case .marsParty:
+            return "Mars Party"
+        case .eternalConstance:
+            return "Eternal Constance"
+        case .japanBlush:
+            return "Japan Blush"
+        case .smilingRain:
+            return "Smiling Rain"
+        case .cloudyApple:
+            return "Cloudy Apple"
+        case .bigMango:
+            return "Big Mango"
+        case .healthyWater:
+            return "Healthy Water"
+        case .amourAmour:
+            return "Amour Amour"
+        case .riskyConcrete:
+            return "Risky Concrete"
+        case .strongStick:
+            return "Strong Stick"
+        case .viciousStance:
+            return "Vicious Stance"
+        case .paloAlto:
+            return "Palo Alto"
+        case .happyMemories:
+            return "Happy Memories"
+        case .midnightBloom:
+            return "Midnight Bloom"
+        case .crystalline:
+            return "Crystalline"
+        case .raccoonBack:
+            return "Raccoon Back"
+        case .partyBliss:
+            return "Party Bliss"
+        case .confidentCloud:
+            return "Confident Cloud"
+        case .leCocktail:
+            return "Le Cocktail"
+        case .riverCity:
+            return "River City"
+        case .frozenBerry:
+            return "Frozen Berry"
+        case .elegance:
+            return "Elegance"
+        case .childCare:
+            return "Child Care"
+        case .flyingLemon:
+            return "Flying Lemon"
+        case .newRetrowave:
+            return "New Retrowave"
+        case .hiddenJaguar:
+            return "Hidden Jaguar"
+        case .aboveTheSky:
+            return "Above The Sky"
+        case .nega:
+            return "Nega"
+        case .denseWater:
+            return "Dense Water"
+        case .chemicAqua:
+            return "Chemic Aqua"
+        case .seashore:
+            return "Seashore"
+        case .marbleWall:
+            return "Marble Wall"
+        case .cheerfulCaramel:
+            return "Cheerful Caramel"
+        case .nightSky:
+            return "Night Sky"
+        case .magicLake:
+            return "Magic Lake"
+        case .youngGrass:
+            return "Young Grass"
+        case .colorfulPeach:
+            return "Colorful Peach"
+        case .gentleCare:
+            return "Gentle Care"
+        case .plumBath:
+            return "Plum Bath"
+        case .happyUnicorn:
+            return "Happy Unicorn"
+        case .fullMetal:
+            return "Full Metal"
+        case .africanField:
+            return "African Field"
+        case .solidStone:
+            return "Solid Stone"
+        case .orangeJuice:
+            return "Orange Juice"
+        case .glassWater:
+            return "Glass Water"
+        case .slickCarbon:
+            return "Slick Carbon"
+        case .northMiracle:
+            return "North Miracle"
+        case .fruitBlend:
+            return "Fruit Blend"
+        case .millenniumPine:
+            return "Millennium Pine"
+        case .highFlight:
+            return "High Flight"
+        case .moleHall:
+            return "Mole Hall"
+        case .earlGray:
+            return "Earl Gray"
+        case .spaceShift:
+            return "Space Shift"
+        case .forestInei:
+            return "Forest Inei"
+        case .royalGarden:
+            return "Royal Garden"
+        case .richMetal:
+            return "Rich Metal"
+        case .juicyCake:
+            return "Juicy Cake"
+        case .smartIndigo:
+            return "Smart Indigo"
+        case .sandStrike:
+            return "Sand Strike"
+        case .norseBeauty:
+            return "Norse Beauty"
+        case .aquaGuidance:
+            return "Aqua Guidance"
+        case .sunVeggie:
+            return "Sun Veggie"
+        case .seaLord:
+            return "Sea Lord"
+        case .blackSea:
+            return "Black Sea"
+        case .grassShampoo:
+            return "Grass Shampoo"
+        case .landingAircraft:
+            return "Landing Aircraft"
+        case .witchDance:
+            return "Witch Dance"
+        case .sleeplessNight:
+            return "Sleepless Night"
+        case .angelCare:
+            return "Angel Care"
+        case .crystalRiver:
+            return "Crystal River"
+        case .softLipstick:
+            return "Soft Lipstick"
+        case .saltMountain:
+            return "Salt Mountain"
+        case .perfectWhite:
+            return "Perfect White"
+        case .freshOasis:
+            return "Fresh Oasis"
+        case .strictNovember:
+            return "Strict November"
+        case .morningSalad:
+            return "Morning Salad"
+        case .deepRelief:
+            return "Deep Relief"
+        case .seaStrike:
+            return "Sea Strike"
+        case .nightCall:
+            return "Night Call"
+        case .supremeSky:
+            return "Supreme Sky"
+        case .lightBlue:
+            return "Light Blue"
+        case .mindCrawl:
+            return "Mind Crawl"
+        case .lilyMeadow:
+            return "Lily Meadow"
+        case .sugarLollipop:
+            return "Sugar Lollipop"
+        case .sweetDessert:
+            return "Sweet Dessert"
+        case .magicRay:
+            return "Magic Ray"
+        case .teenParty:
+            return "Teen Party"
+        case .frozenHeat:
+            return "Frozen Heat"
+        case .gagarinView:
+            return "Gagarin View"
+        case .fabledSunset:
+            return "Fabled Sunset"
+        case .perfectBlue:
+            return "Perfect Blue"
+        }
+    }
+}
